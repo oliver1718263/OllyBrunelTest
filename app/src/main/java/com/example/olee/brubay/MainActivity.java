583cd48792c460e7874cf63af395917cbd9b9b23
oliver1718263 <33158520+oliver1718263@users.noreply.github.com>
2019-01-08 02:30:07 +0000

    This is my first commit.

---
+++ app/src/main/java/com/example/olee/brubay/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.olee.brubay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected Button myButton;
+    String myString = "This is a notification";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = (Button)findViewById(R.id.buttonID);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), myString, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
